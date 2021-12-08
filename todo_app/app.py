87e2a068b555fed0feda92db323a5eb262d6f84e
Nirav Shilu <Nirav.Shilu@capita.com>
2021-12-08 16:15:11 +0000

    Add to To Do List

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 from flask import render_template
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items, add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -12,3 +12,14 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template("index.html", items = items)
+
+@app.route('/addItem', methods=['POST'])
+
+def index_addItem():
+
+    title=request.form.get('title')
+
+    add_item(title)
+
+    return redirect('/')
+
