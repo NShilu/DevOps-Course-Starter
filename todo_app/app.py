e16e32e9d0d972f5d6321b72311df03107331482
Nirav Shilu <Nirav.Shilu@capita.com>
2022-03-31 16:29:59 +0100

    Trello Complete Card

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,8 +2,7 @@ from flask import Flask, redirect, url_for, request
 from flask import render_template
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import add_item
-from todo_app.data.trello_items import get_items
+from todo_app.data.trello_items import get_items, add_item, complete_card
 
 
 app = Flask(__name__)
@@ -16,10 +15,13 @@ def index():
     return render_template("index.html", items = items)
 
 @app.route('/addItem', methods=['POST'])
-def add_item():
+def new_item():
     title=request.form.get('title')
     add_item(title)
     return redirect('/')
 
-    
-
+@app.route('/completeItem', methods=['POST'])
+def complete_item():
+    id=request.form.get('id')
+    complete_card(id)
+    return redirect('/')
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,22 +1,41 @@
-from flask import session, Flask
+from flask import session,Flask
 import requests
-import dotenv 
 import os
+from todo_app.flask_config import Config
+import json
+
+api_key = os.getenv('API_KEY')
+api_server_token = os.getenv('API_SERVER_TOKEN')
+
+class Item:
+    def __init__(self, id, title, status):
+        self.id = id
+        self.title = title
+        self.status = status
+
+    @classmethod
+    def from_trello_card(Item, card, list):
+        return Item(card['id'], card['name'], list['name'])  
 
 def get_items():
-    dotenv.load_dotenv()
-    board_id = os.getenv('API_TRELLO_BOARDID')
-    api_key=os.getenv('API_KEY')
-    api_server_token=os.getenv('API_SERVER_TOKEN')
-    reqUrl = f"https://api.trello.com/1/boards/{board_id}/lists"
-    params = {
-        "key": api_key,
-        "token": api_server_token,
-        "cards":"open"
-    }
+    items=[]
+    cards_param = {'cards': 'open','key':api_key,'token':api_server_token}
+    response = requests.get('https://trello.com/1/boards/621e24addce3fd2cedb1326e/lists', params=cards_param)
+    for my_list in response.json():
+        for card in my_list['cards']:
+           item = Item.from_trello_card(card, my_list)
+           items.append(item)
+    return items
 
-    response = requests.get(reqUrl, params=params)
-    response_json= response.json()
-    cards = response_json[0]['cards']
-    return cards
+def add_item(title):
     
+    cards_param = {'key':api_key,'token':api_server_token,'idList':'621e24addce3fd2cedb1326f','name':title}
+    response=requests.post('https://api.trello.com/1/cards', params=cards_param)
+    response.raise_for_status()
+    
+def complete_card(id):
+    cards_param = {'key':api_key,'token':api_server_token, 'idList':'621e24addce3fd2cedb13271'}
+    response=requests.put(f'https://api.trello.com/1/cards/{id}',params=cards_param)
+    response.raise_for_status()
+
+
